df36dca803a787fba5345a0c5d157deace30c903
Paulo Nadales <p.nadales.j@gmail.com>
2024-08-30 14:03:17 -0400

    agregar ruta aceptada de render

--- middelwares/cors.js
+++ middelwares/cors.js
@@ -4,6 +4,7 @@ const ACCEPTED_ORIGINS = [
   "http://localhost:3000",
   "http://localhost:5500",
   "http://127.0.0.1:5500",
+  "https://url-shortener-07ac.onrender.com",
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
